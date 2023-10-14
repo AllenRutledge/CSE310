4fcd61e3804ab73a49daa56ae8d7fc9a8930bbf7
Allen Rutledge <allencr1998@gmail.com>
2023-10-14 17:33:54 -0600

    Completed module02

---
+++ module02/README.md
@@ -0,0 +1,17 @@
+# Overview
+
+This application will play music from a provided list of songs. You can adjust the volume, position of the music, pause, resume, go to the next song, and go to the previous song. If the current song ends, it will automatically transition to the next song. Alongside the buttons and sliders, the player will display the song's name, the song's artist, and which song out the list is playing.
+
+I created this application to challenge myself and expand my knowledge of web programming. Additionally, I wanted to know about web programming to be more helpful with my team activity. I ended up unintentionally making this harder by not realizing I had to do 3 different files for one program with their own unique languages, but I still had a lot of fun with this.
+
+[Javascript Music Player Tutorial]()
+
+# Development Environment
+
+The programs used were ==Visual Studio Code by Microsoft== and ==Microsoft Edge by Microsoft== (for testing).
+
+The programming languages I used are ==HTML, CSS,== and ==Javascript==.
+
+# Useful Website
+
+* [Geeks For Geeks](https://www.geeksforgeeks.org/create-a-music-player-using-javascript/)
---
+++ module02/player.js
@@ -0,0 +1,134 @@
+// Select everything and assign it here
+let trackSpot = document.querySelector(".song-spot");
+let songName = document.querySelector(".song-name");
+let songArtist = document.querySelector(".song-artist");
+
+let playButton = document.querySelector(".playpause-song");
+let nextButton = document.querySelector(".next-song");
+let prevButton = document.querySelector(".prev-song");
+
+let seekSlider = document.querySelector(".seek-slider");
+let volumeSlider = document.querySelector(".volume-slider");
+let timeNow = document.querySelector(".time-now");
+let totalDuration = document.querySelector(".total-duration");
+
+// Global stuff
+let index = 0;
+let isPlaying = false;
+let updateTimer;
+
+// Make program play audio
+let songNow = document.createElement('audio');
+// List of songs
+let songs = [
+{   name: "Her And I (Slow Jam 2)",
+	artist: "King Gizzard and the Lizard Wizard",
+    path: "Her_and_I.mp3"},
+
+{   name: "Voyager",
+    artist: "Jasper Bryne",
+	path: "Voyager.mp3",},
+
+{	name: "Simma Hem",
+    artist: "Riddarna",
+	path: "Simma_Hem.mp3",},
+
+{	name: "Crystals",
+    artist: "M|O|O|N",
+	path: "Crystals.mp3",}
+];
+function loadSong(index) {
+    // Clear previous timer
+    clearInterval(updateTimer);
+    resetValues();
+    
+    // Load song
+    songNow.src = songs[index].path;
+    songNow.load();
+    
+    // Update song details
+    songName.textContent = songs[index].name;
+    songArtist.textContent = songs[index].artist;
+    trackSpot.textContent = 
+        "Song " + (index + 1) + " of " + songs.length;
+    
+    // 1-second interval for updating seek slider.
+    updateTimer = setInterval(seekUpdate, 1000);
+    
+    // Go to next song when done
+    songNow.addEventListener("ended", nextSong);
+}
+// Reset to default values
+function resetValues() {
+    timeNow.textContent = "00:00";
+    totalDuration.textContent = "00:00";
+    seekSlider.value = 0;
+}
+function playPause() {
+    // Switch between play and pause
+    if (!isPlaying) playSong();
+    else pauseSong();
+}   
+function playSong() {
+    // Play loaded song
+    songNow.play();
+    isPlaying = true;
+    // Replace play with pause
+    playButton.innerHTML = '<p class="play">II</p>';
+}   
+function pauseSong() {
+    // Pause song
+    songNow.pause();
+    isPlaying = false;
+    // Replace pause with play
+    playButton.innerHTML = '<p class="play">[></p>';
+}
+function nextSong() {
+    // Loop forward to first song if you press forward on last song
+    if (index < songs.length - 1)
+        index += 1;
+    else index = 0;    
+    // Load and play song
+    loadSong(index);
+    playSong();
+}    
+function prevSong() {
+    // Loop back to last song if you press back on first song
+    if (index > 0)
+        index -= 1;
+    else index = songs.length - 1;   
+    // Load and play song
+    loadSong(index);
+    playSong();
+}
+
+function seekTo() {
+    // Percentage-based location of where you are in the song
+    seeking = songNow.duration * (seekSlider.value / 100);
+    songNow.currentTime = seeking;
+}
+function setVolume() {
+    // Percentage-based location of volume
+    songNow.volume = volumeSlider.value / 100;
+}
+function seekUpdate() {
+    let seekPosition = 0;   
+    // Is the song's duration legible?
+    if (!isNaN(songNow.duration)) {
+        seekPosition = songNow.currentTime * (100 / songNow.duration);
+        seekSlider.value = seekPosition;
+        // Remaining time, then duration
+        let currentMinutes = Math.floor(songNow.currentTime / 60);
+        let currentSeconds = Math.floor(songNow.currentTime - currentMinutes * 60);
+        let durationMinutes = Math.floor(songNow.duration / 60);
+        let durationSeconds = Math.floor(songNow.duration - durationMinutes * 60);
+        // Add a zero to the single digit time values
+        if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+        if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+        if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+        if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+        // Display the updated duration
+        timeNow.textContent = currentMinutes + ":" + currentSeconds;
+        totalDuration.textContent = durationMinutes + ":" + durationSeconds;
+    }
+}
