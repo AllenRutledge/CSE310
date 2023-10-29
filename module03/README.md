68b893de76a2927aa8ad252f094ff4d310edb9e0
Allen Rutledge <allencr1998@gmail.com>
2023-10-28 20:59:08 -0600

    Module03: fixed bug where retry ended the game

--- module03/README.md
+++ module03/README.md
@@ -1,6 +1,6 @@
 # Overview
 
-This application starts you off in a text adventure game. The player will have to navigate using keywords about what to do next in the story. This game is designed to be played more than once, having a total of 12 endings. In terms of code, the game is mainly just if else statements and calls for input, alongside slight pauses in the game.
+This application starts you off in a text adventure game. The player will have to navigate using keywords about what to do next in the story. This game is designed to be played more than once, having a total of 12 endings. In terms of code, the game is mainly just if else statements and calls for input, alongside slight pauses in the game. Upon getting an ending, the game resets.
 
 I created this application to challenge myself and expand on something I did before and refine it now that I know more about coding.
 
--- module03/adventure.py
+++ module03/adventure.py
@@ -1,9 +1,6 @@
 import time
 
 invalidText = "Sorry, I don't quite understand. Please select the available options. "
-
-xChoice = input("You wake up in a pitch black forest, no recollection of how you ended up in such a place. Before you is a box of MATCHES and a FLASHLIGHT You pick up the metal flashlight, and the box of matches. For now, you put the matches in your pocket, and push the button to turn the flashlight on. As the area in front of you illuminates, you realize after pocketing the matches that you're in clothes that aren't your own: As you turn the flashlight on yourself, you see that you're in a gray jumpsuit and heavy boots. You appear to be in the center of a dirt pathway. Directions leading LEFT and RIGHT. Additionally, there appears to be a direction FORWARD that leads to the distant mouth of a cave. Which direction will you go? ")
-
 def ending(num):
     if num == 1:
         time.sleep(0.5)
@@ -54,66 +51,87 @@ def ending(num):
         print("You find a car in the parking lot. This is definitely illegal, but you're already tresspassing and you're a little desperate. You take the car and drive home. Not wanting to be an idiot, you park the car somewhere nearby instead of in your driveway. The next day, you're greeted by two cops. You say you don't know anything, clearly lying. Unfortunately, the car had a camera in it, and you're sent to prison for theft.")
         print("<----ENDING 12--->")
 
-# DIRECTIONS OPTIONS ------------------------------------------------
-if xChoice.upper() == "LEFT":
-    time.sleep(0.5)
-    cabinChoice = input("After what feels like 10 minutes, you find a cabin with only one candle lit on the porch. At the door you find a sign that asks you to knock in rather crude handwriting. Do you KNOCK, or SNEAK inside? ")
-    # CABIN OPTIONS ------------------------------------------------------
-    if cabinChoice.upper() == "KNOCK":
+while True:
+    xChoice = input("You wake up in a pitch black forest, no recollection of how you ended up in such a place. Before you is a box of MATCHES and a FLASHLIGHT You pick up the metal flashlight, and the box of matches. For now, you put the matches in your pocket, and push the button to turn the flashlight on. As the area in front of you illuminates, you realize after pocketing the matches that you're in clothes that aren't your own: As you turn the flashlight on yourself, you see that you're in a gray jumpsuit and heavy boots. You appear to be in the center of a dirt pathway. Directions leading LEFT and RIGHT. Additionally, there appears to be a direction FORWARD that leads to the distant mouth of a cave. Which direction will you go? ")
+
+    # DIRECTIONS OPTIONS ------------------------------------------------
+    if xChoice.upper() == "LEFT":
         time.sleep(0.5)
-        inCabinChoice = input("You shakily knock on the door, and after a while, you hear footsteps for the door that soon opens, as you see the person, you immediately question whether or not you should run. The 8-foot tall person at the door is in rugged, mildly dilapidated clothes and a haniwa mask. The person stares at you, waiting for you to speak. Do you SPEAK, or do you RUN? ")
-        # CABIN MAN OPTIONS ------------------------------------------------------
-        if inCabinChoice.upper() == "SPEAK":
-            ending(1)
-        elif inCabinChoice.upper() == "RUN":
-            ending(2)
-        else:
-            inCabinChoice = input(invalidText)
-    elif cabinChoice.upper() == "SNEAK":
-        ending(3)
-    else:RIGHT
-        cabinChoice = input(invalidText)
-    # CAR OPTIONS ------------------------------------------------------
-elif xChoice.upper() == "RIGHT":
-    time.sleep(0.5)
-    carChoice = input("You begin walking to the right, and soon find a car with the engine still running. The keys are in the ignition, and the door is open slightly. Do you WAIT for the owner of the car, or DRIVE out of the forest? ")
-    if carChoice.upper() == "WAIT":
-        ending(4)
-    elif carChoice.upper() == "DRIVE":
-        ending(5)
-    else:
-        carChoice = input(invalidText)
-    # CAVE OPTIONS ------------------------------------------------------
-elif xChoice.upper() == "FORWARD":
-    time.sleep(0.5)
-    caveChoice = input("You trek past the trees, and find the mouth of a cave. Inside the cave, you hear a rhythmic, metallic clang from deep within. Do you ENTER, CALL to see if you get a response, or LEAVE to go past it? ")
-    if caveChoice.upper() == "ENTER":
-        ending(6)
-    elif caveChoice.upper() == "CALL":
-        ending(7)
-    elif caveChoice.upper() == "LEAVE":
-        time.sleep(0.5)
-        # ROAD OPTIONS ------------------------------------------
-        roadChoice = input("You go past it, realizing a cave is probably not the place to explore without proper equipment. You keep walking through the forest and find some road. You have a few ideas: You can either HITCHHIKE, walk LEFT alongside the road, or walk RIGHT alongside the road. ")
-        if roadChoice.upper() == "HITCHHIKE":
-            ending(8)
-        elif roadChoice.upper() == "LEFT":
-            ending(9)
-        elif roadChoice.upper() == "RIGHT":
-            time.sleep(0.5)
-            # FACTORY OPTIONS -----------------------------------
-            factoryChoice = input("You start your walk down the right side of the road, and eventually come across some sort of industrial building. It's not the best look to be by a factory in the dead of night. Do you keep WALKing, ENTER the facility in hopes of finding someone, or do you SNEAK in to find another way of getting home? ")
-            if factoryChoice.upper() == "WALK":
-                ending(10)
-            elif factoryChoice.upper() == "ENTER":
-                ending(11)
-            elif factoryChoice.upper() == "SNEAK":
-                ending(12)
+        while True:
+            cabinChoice = input("After what feels like 10 minutes, you find a cabin with only one candle lit on the porch. At the door you find a sign that asks you to knock in rather crude handwriting. Do you KNOCK, or SNEAK inside? ")
+            # CABIN OPTIONS ------------------------------------------------------
+            if cabinChoice.upper() == "KNOCK":
+                time.sleep(0.5)
+                while True:
+                    inCabinChoice = input("You shakily knock on the door, and after a while, you hear footsteps for the door that soon opens, as you see the person, you immediately question whether or not you should run. The 8-foot tall person at the door is in rugged, mildly dilapidated clothes and a haniwa mask. The person stares at you, waiting for you to speak. Do you SPEAK, or do you RUN? ")
+                    # CABIN MAN OPTIONS ------------------------------------------------------
+                    if inCabinChoice.upper() == "SPEAK":
+                        ending(1)
+                        break
+                    elif inCabinChoice.upper() == "RUN":
+                        ending(2)
+                        break
+                    else:
+                        print(invalidText)
+            elif cabinChoice.upper() == "SNEAK":
+                ending(3)
+                break
             else:
-                factoryChoice = input(invalidText)
-        else:
-            roadChoice = input(invalidText)
+                print(invalidText)
+        # CAR OPTIONS ------------------------------------------------------
+    elif xChoice.upper() == "RIGHT":
+        time.sleep(0.5)
+        while True:
+            carChoice = input("You begin walking to the right, and soon find a car with the engine still running. The keys are in the ignition, and the door is open slightly. Do you WAIT for the owner of the car, or DRIVE out of the forest? ")
+            if carChoice.upper() == "WAIT":
+                ending(4)
+                break
+            elif carChoice.upper() == "DRIVE":
+                ending(5)
+                break
+            else:
+                print(invalidText)
+        # CAVE OPTIONS ------------------------------------------------------
+    elif xChoice.upper() == "FORWARD":
+        time.sleep(0.5)
+        while True:
+            caveChoice = input("You trek past the trees, and find the mouth of a cave. Inside the cave, you hear a rhythmic, metallic clang from deep within. Do you ENTER, CALL to see if you get a response, or LEAVE to go past it? ")
+            if caveChoice.upper() == "ENTER":
+                ending(6)
+                break
+            elif caveChoice.upper() == "CALL":
+                ending(7)
+                break
+            elif caveChoice.upper() == "LEAVE":
+                time.sleep(0.5)
+            # ROAD OPTIONS ------------------------------------------
+            while True:
+                roadChoice = input("You go past it, realizing a cave is probably not the place to explore without proper equipment. You keep walking through the forest and find some road. You have a few ideas: You can either HITCHHIKE, walk LEFT alongside the road, or walk RIGHT alongside the road. ")
+                if roadChoice.upper() == "HITCHHIKE":
+                    ending(8)
+                    break
+                elif roadChoice.upper() == "LEFT":
+                    ending(9)
+                    break
+                elif roadChoice.upper() == "RIGHT":
+                    time.sleep(0.5)
+                # FACTORY OPTIONS -----------------------------------
+                while True:
+                    factoryChoice = input("You start your walk down the right side of the road, and eventually come across some sort of industrial building. It's not the best look to be by a factory in the dead of night. Do you keep WALKing, ENTER the facility in hopes of finding someone, or do you SNEAK in to find another way of getting home? ")
+                    if factoryChoice.upper() == "WALK":
+                        ending(10)
+                        break
+                    elif factoryChoice.upper() == "ENTER":
+                        ending(11)
+                        break
+                    elif factoryChoice.upper() == "SNEAK":
+                        ending(12)
+                        break
+                    else:
+                        print(invalidText)
+                else:
+                    print(invalidText)
+            else:
+                print(invalidText)
     else:
-        caveChoice = input(invalidText)
-else:
-    xChoice = input(invalidText)
+        print(invalidText)
